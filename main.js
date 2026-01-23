3f03830e0e81f66e75c728206a9b56729fa7413e
Gaffuri01 <joao_pgaffuri@hotmail.com>
2026-01-23 12:19:17 -0300

    add icon and fix

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
 });
+
+document.getElementById("btnTopo").addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 24,
+    speed: 900,
+
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+
+    breakpoints: {
+      320: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+    },
+  });
+
